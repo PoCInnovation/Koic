3b9d3cc194fc72cd439048ec26c27fd6db878901
pr0m3th3usEx <thomas1.michel@epitech.eu>
2021-05-02 22:16:37 +0200

    Add submodule yolov5

--- streaming/client.py
+++ streaming/client.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
 import cv2
+import sys
+import os
 
+IP = "192.168.1.26"
+PORT = 9160
 
-IP = "192.168.1.5"
-PORT = 1425
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 
 client = cv2.VideoCapture("rtsp://{}:{}/out.h264".format(IP, PORT))
+
+if not client.isOpened():
+    sys.exit(1)
 
 while True:
     try:
         ret, frame = client.read()
         if ret:
+
             cv2.imshow('FRAME', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
