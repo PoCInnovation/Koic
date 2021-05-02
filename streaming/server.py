677dbc6a92951c31d4eef28b257c8690f8b69f53
pr0m3th3usEx <thomas1.michel@epitech.eu>
2021-05-02 15:22:47 +0200

    Add draft test subprocess vlc

--- streaming/server.py
+++ streaming/server.py
@@ -1,7 +1,9 @@
 import sys
+import time
+import socket
 import subprocess
 from io import BytesIO
-from picamera import PiCamera
+from picamera import picamera
 
 
 class CustomOutput:
@@ -14,17 +16,41 @@ class CustomOutput:
     def flush(self):
         print(self.content)
 
-PORT = 1425
+class StreamBroadcaster:
+    PORT = 1425
+    VLC_ARGS = [
+        "cvlc",
+        "-vvv",
+        "stream:///dev/stdin",
+        "--sout",
+        "'#rtp{{sdp=rtsp://:{}/}}'".format(PORT),
+        ":demux=h264"
+    ]
+
+    def __init__(self):
+        self.vlc = subprocess.Popen(VLC_ARGS, stdin=subprocess.PIPE)
+        self.buffer = b""
+        print("[+] VLC Subprocess launched - streaming on rtsp://{}:{}/".format(
+                socket.gethostbyname(socket.gethostname()),
+                PORT
+            )
+        )
+
+    def write(self, s):
+        self.buffer += s
+
+    def flush(self):
+        self.vlc.stdin += self.buffer
+        self.buffer = b""
+
+    def close(self):
+        self.vlc.terminate()
+
+
 
 # raspivid -o - -t 0 -n => Opening camera and starting recoding information
-VLC_ARGS = [
-    "cvlc",
-    "-vvv",
-    "stream:///dev/stdin",
-    "--sout",
-    "'#rtp{{sdp=rtsp://:{}/}}'".format(PORT),
-    ":demux=h264"
-]
+
+
 
 print("------------------------------------")
 print("  KoiC - Pi Camera Streaming v0.1   ")
@@ -37,11 +63,14 @@ with PiCamera() as camera:
     
     # print("[+] VLC broadcasting video on port {}".format(PORT))
     # camera.start_recording(vlcBroadcaster.stdin)
-    camera.start_recording(CustomOutput(), format="h264")
+    stream = StreamBroadcaster()
+    camera.start_recording(stream, format="h264")
+    time.sleep(2)
+    print("[+] Start recording")
     try:
         while True:
             camera.wait_recording(1)
+            stream.flush()
     finally:
         camera.stop_recording()
-
-    camera.stop_recording()
+        stream.close()
