c0947fc8f543e295a65a9c0bc092395f5d2c3acb
pr0m3th3usEx <thomas1.michel@epitech.eu>
2021-05-26 20:06:57 +0200

    feat: add log of object detected int he image

--- streaming/v2/detector.py
+++ streaming/v2/detector.py
@@ -1,5 +1,6 @@
 import torch
 import cv2
+import logging
 
 class Detector:
     FONT = cv2.FONT_HERSHEY_SIMPLEX
@@ -9,12 +10,20 @@ class Detector:
 
     def __init__(self, model_name):
         self.model = torch.hub.load('ultralytics/yolov5', model_name)
+        logging.basicConfig(
+            filename="log/info.log",
+            format="[+] %(name)s - %(asctime)s: %(message)s",
+            level=logging.INFO,
+            datefmt='%d-%b-%y %H:%M:%S'
+        )
 
     def get_predicted_boxes(self, frame):
         result = self.model(frame)
-        result.print()
+        # print(dir(result))
+        # result.print()
         points_data = result.xyxy[0].numpy()
         for xmin, ymin, xmax, ymax, confidence, class_nb in points_data:
+            logging.info(f"New object found : {class_nb} (label_name)")
             frame = cv2.rectangle(
                 frame.copy(),
                 (int(xmin), int(ymin)),
