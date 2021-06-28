195435535835ad58579caa926acc143e928d12f8
Inès <ines.maaroufi@epitech.eu>
2021-06-28 10:06:35 +0200

    [add] launch screen

--- koic-app/components/screens/settings.js
+++ koic-app/components/screens/settings.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import {View, Text, StyleSheet, Dimensions, ImageBackground } from 'react-native'
 import { Header } from '../utils/graph'
 import { style } from '../utils/style'
+import { Linking } from 'react-native';
+
+function Contact () {
+    
+}
 
 export default class Settings extends React.Component {
     render() {
