df9c51065f72a39551407d3707ea3adb3cae081d
pr0m3th3usEx <thomas1.michel@epitech.eu>
2021-07-01 19:15:53 +0200

    feat: creation of all routes draft

--- API/routes/animals.py
+++ API/routes/animals.py
@@ -1,3 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from markupsafe import escape
 
 animals = Blueprint('/api/animals', __name__)
+
+def get_detection_info():
+    return "GET /animals"
+
+def register_new_detection():
+    return "POST /api/animals"
+
+@animals.route('/', methods=['GET', 'POST'])
+def handle_animals_requests():
+    if request.method == 'POST':
+        return register_new_detection()
+    else:
+        return get_detection_info()
+
+@animals.route('/<name>', methods=['GET'])
+def get_animal_data(name):
+    return escape(name)
--- API/routes/stream.py
+++ API/routes/stream.py
@@ -1,3 +1,7 @@
 from flask import Blueprint
 
 stream = Blueprint('/stream', __name__)
+
+@stream.route('/', methods=['GET'])
+def stream():
+    return "Stream available soon"
