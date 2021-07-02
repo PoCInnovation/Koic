f4c205a4ee98fd47d5f6a54fe6dc49819f9ced4d
pr0m3th3usEx <thomas1.michel@epitech.eu>
2021-07-02 13:17:34 +0200

    feat: add Animal model

---
+++ API/models/AnimalModel.py
@@ -0,0 +1,26 @@
+from sqlalchemy.dialects.postgresql import UUID
+from enum import Enum
+from app import db
+from datetime import datetime
+import uuid
+
+class Animals(Enum):
+    raven = 0,
+    boar = 1
+
+class Animal(db.Model):
+    __tablename__ = "Animal"
+
+    id = db.Column('id', UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = db.Column('name', db.Enum(Animals))
+    detected_at = db.Column('detected_at', db.DateTime())
+    created_at = db.Column('created_at', db.DateTime(), default=datetime.now)
+
+# Database Architecture
+
+# Animals:
+
+# ID            uuid
+# NAME          Enum (Raven, Boar)
+# DETECTED_AT   DateTime
+# CREATED_AT    DateTime    NOW()
---
+++ API/models/__init__.py
@@ -0,0 +1,1 @@
+from models.AnimalModel import Animal
