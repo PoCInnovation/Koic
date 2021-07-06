27b91e3aff3e825a1a05eb81ed5879a23beab674
pr0m3th3usEx <thomas1.michel@epitech.eu>
2021-07-06 19:28:27 +0200

    feat: add jpeg stream based on kafka

--- API/routes/stream.py
+++ API/routes/stream.py
@@ -1,7 +1,24 @@
-from flask import Blueprint
+from kafka import KafkaConsumer
+from flask import Blueprint, Response
 
 stream = Blueprint('/stream', __name__)
 
-@stream.route('/:id', methods=['GET'])
-def stream_cam():
-    return "Stream available soon"
+def get_video_stream(id):
+    consumer = KafkaConsumer(f"CAMERA_{int(id)}", bootstrap_servers=["localhost:9092"])
+
+    for msg in consumer:
+        yield(
+            b'--frame\r\n'
+            b'Content-Type: image/jpg\r\n\r\n' + msg.value + b'\r\n\r\n'
+        )
+
+
+@stream.route('/<id>', methods=['GET'])
+def stream_cam(id):
+    try:
+        return Response(
+            get_video_stream(int(id)),
+            mimetype='multipart/x-mixed-replace; boundary=frame'
+        )
+    except Exception as e:
+        return "No stream available", 404
