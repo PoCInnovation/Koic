f3a90ab956f8466ee1ede251645c3e0d9ccb9c06
Inès <ines.maaroufi@epitech.eu>
2021-07-26 00:49:32 +0200

    [update] about on applciation

--- koic-app/components/screens/About.js
+++ koic-app/components/screens/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {View, Text, StyleSheet } from 'react-native'
 import { Header } from '../utils/graph'
+import Hyperlink from 'react-native-hyperlink'
 import { style } from '../utils/style'
 
 function Infos () {
@@ -8,11 +9,15 @@ function Infos () {
         <View>
             <Text style={styles.Title}>
             What is this project ?</Text>
-            <Text style={styles.Text}>Koic is a connected scarecrow that can be used to scare away crop pests such as crows and wild boars in different ways.</Text>
+            <Text style={styles.Text}>Koic is a connected scarecrow who is equipped with cameras that film the terrain in real time.
+            The filmed images are then analysed. Depending on the animal that is hit,
+            the scarecrow will choose a suitable way to repel it in a natural way.</Text>
             <Text style={styles.Title}>Who are we?</Text>
             <Text style={styles.Text}>We are students from the PoC innovations association.</Text>
             <Text style={styles.Title}>Contact us</Text>
-            <Text style={styles.Text}>link</Text>
+            <Text style={styles.Text}>ines.maaroufi@epitech.eu</Text>
+            <Text style={styles.Text}>thomas.michel@epitech.eu</Text>
+            <Text style={styles.Text}>contact@poc-innovation.fr</Text>
         </View>
     )
 }
@@ -30,7 +35,7 @@ export default class About extends React.Component {
 
 const styles = StyleSheet.create({
     Title: {
-        top: '20%', 
+        top: '15%', 
         marginLeft: '2%',
         alignItems: 'flex-start',
         justifyContent: 'center',
@@ -39,9 +44,12 @@ const styles = StyleSheet.create({
         color: '#583cfb'
     },
     Text: {
-        color: '#583cfb',
+        color: 'black',
         fontWeight: 'bold',
         marginTop : '5%',
-        bottom : '-30%'
+        marginLeft: '3%',
+        marginRight: '1%',
+        bottom : '-10%',
+        top: '12%'
     }
 })
