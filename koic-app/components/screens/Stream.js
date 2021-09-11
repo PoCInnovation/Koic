3714c10e79930f45fe0d5137839c69468e10595f
Inès <ines.maaroufi@epitech.eu>
2021-09-11 19:32:30 +0200

    [add] style for button add and remove

--- koic-app/components/screens/Stream.js
+++ koic-app/components/screens/Stream.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { View,  SafeAreaView, Button, ScrollView } from 'react-native'
+import { View,  SafeAreaView, Button, ScrollView, Styleheet } from 'react-native'
 // import { Video } from 'expo-av'
 import { Header } from '../utils/graph'
 import { style } from '../utils/style.js'
@@ -12,56 +12,37 @@ function Stream(props) {
     return (
         <>
             <Header text='Stream'/>
-            <SafeAreaView style={styles.container}>
-                <ScrollView style={styles.scrollView}>
+            <SafeAreaView style={style.viewContainerButton}>
+                <ScrollView style={style.scrollView}>
                     <IntruderViews nbViews={nbViews}/>
                 </ScrollView>
             </SafeAreaView>
-            <View style={styles.container}>
-                <View style={styles.buttonContainer}>
+            <View style={style.viewContainerButton}>
+                <View style={style.button}>
                 <Button
-                    style={style.button}
-                    title={'Add View'}
+                    title={'Add'}
+                    color={'#6c53f8'}
+                    type="outline"
                     onPress={() => {setNbViews(nbViews + 1)}}
-                    accessibilityLabel="Learn more about this purple button"
+                    accessibilityLabel="Add new view stream"
                 />
                 </View>
-                <View style={styles.buttonContainer}>
+                <View style={style.button}>
                 <Button
-                    style={style.button}
-                    title={'Remove View'}
-                    color={'#841584'}
+                    title={'Remove'}
+                    type="outline"
+                    color={'#6c53f8'}
                     onPress={() => {
                         if (nbViews != 1) {
                             setNbViews(nbViews - 1)
                         }
                     }}
-                    accessibilityLabel="Learn more about this purple button"
+                    accessibilityLabel="Remove new view stream"
                 />
                 </View>
             </View>
         </>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 250,
-    },
-    container: {
-      flex: 1,
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    scrollView: {
-        backgroundColor: 'pink',
-        marginHorizontal: 20,
-    },
-    buttonContainer: {
-      flex: 1,
-    }
-});
 
 export default Stream
